24592bfb53aef9e7f8c5517fbb72c64879ddc981
Giorgi <stichy.dl@gmail.com>
2022-12-26 19:01:14 +0400

    add comment

--- web/modules/custom/joblisting/src/Controller/JobController.php
+++ web/modules/custom/joblisting/src/Controller/JobController.php
@@ -2,47 +2,56 @@
 /**
  * @file
  * Fetches data from DB and sends it to frontpage.
+ * cool
  */
 
- namespace Drupal\joblisting\Controller;
+namespace Drupal\joblisting\Controller;
 
- use Drupal\Core\Controller\ControllerBase;
- use Drupal\joblisting\Service\EntityQueryService;
- use Drupal\joblisting\Service\Utils;
- use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\joblisting\Service\EntityQueryService;
+use Drupal\joblisting\Service\Utils;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
- class JobController extends ControllerBase {
+class JobController extends ControllerBase
+{
+    protected EntityQueryService $query;
+    protected Utils $utils;
 
-   protected EntityQueryService $query;
-   protected Utils $utils;
+    public function __construct(
+        EntityQueryService $entityQueryService,
+        Utils $utils
+    ) {
+        $this->query = $entityQueryService;
+        $this->utils = $utils;
+    }
 
-   public function __construct(EntityQueryService $entityQueryService, Utils $utils) {
-     $this->query = $entityQueryService;
-     $this->utils = $utils;
-   }
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('joblisting.query'),
+            $container->get('joblisting.utils')
+        );
+    }
 
-   public static function create(ContainerInterface $container) {
-     return new static(
-       $container->get('joblisting.query'),
-       $container->get('joblisting.utils')
-     );
-   }
-
-
-
-    private function getJobs() {
+    private function getJobs()
+    {
         $title = \Drupal::request()->request->get('title');
         $location = \Drupal::request()->request->get('location');
         $locationS = \Drupal::request()->request->get('locationS');
         $fullTime = \Drupal::request()->request->get('fullTime');
 
-        $results = $this->query->fetchJobs($title, $location, $locationS, $fullTime);
+        $results = $this->query->fetchJobs(
+            $title,
+            $location,
+            $locationS,
+            $fullTime
+        );
 
         return $this->utils->loadJobs($results);
     }
 
-    public function content() {
-
+    public function content()
+    {
         $jobs = $this->getJobs();
 
         return [
@@ -50,4 +59,4 @@
             '#jobs' => $jobs,
         ];
     }
- }
+}
